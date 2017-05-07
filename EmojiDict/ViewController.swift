f24574ce4056f4abd899b3927c8da3199540789d
LDENV Ruby <dietertim@mac.com>
2017-05-07 15:27:17 +0200

    Show table rows with same emoji

--- EmojiDict/ViewController.swift
+++ EmojiDict/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+
+    @IBOutlet weak var dthottableview: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        dthottableview.dataSource = self
+        dthottableview.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Tim's row 😎🐪"
+        return cell
+    
     }
 
     override func didReceiveMemoryWarning() {
